4acab78519ca0544f218e9f26ecd8c622dfb795d
Mauricio <mauricio.maigre@gmail.com>
2019-08-08 13:34:35 -0700

    ports

--- api/sequelize/server.js
+++ api/sequelize/server.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log("App listening on port 3000!"));
+app.listen(process.env.PORT || 8080, () => console.log("App listening on port 3000!"));
 
 app.get("/", (req,res)=>{
     res.send("Server running")
