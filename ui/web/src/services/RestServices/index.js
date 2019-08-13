00a50917264baf658b3b9cddee10cefd8749c03d
Carlos Saavedra <csaavedra@arkusnexus.com>
2019-08-13 16:59:10 -0700

    Integrate the details contact service with the front

--- ui/web/src/services/RestServices/index.js
+++ ui/web/src/services/RestServices/index.js
@@ -3,17 +3,17 @@ import axios from 'axios';
 export default class RestServices{
     constructor(){
         this.http = axios.create({
-            baseUrl: process.env.VUE_APP_BASE_URL,
-            headers:{ Authorization: `Bearer ${proccess.env.VUE_APP_ACCESS_TOKEN}` }
+            baseURL: `${process.env.VUE_APP_BASE_URL}`,
+            headers:{ Authorization: `Bearer ${process.env.VUE_APP_ACCESS_TOKEN}` }
         })
     }
   
     getContacts(){
-        return this.http.get("contacts"); 
+        return this.http.get("/contacts"); 
     }
   
     createContact (contact){
-        return axios.post("contacts", contact);
+        return this.http.post("/contacts", contact);
     }
   
     getContact(id) {
--- ui/web/src/views/ContactDetails.vue
+++ ui/web/src/views/ContactDetails.vue
@@ -1,8 +1,9 @@
 <template>
-  <v-container fluid grid-list-lg px-10 pt-5 fill-height>
+  <v-container fluid grid-list-lg px-10 pt-5 fill-height class="container container-details">
     <v-layout column wrap>
       <v-flex d-flex xs1>
         <h1>
+          <v-icon large class="back-icon" color="white" @click="handleBack()">arrow_back</v-icon>
           <span class="form-title">{{contact.name}}</span>
         </h1>
       </v-flex>
@@ -88,14 +89,12 @@
           </v-flex>
         </v-layout>
       </v-flex>
-      <v-flex v-if="contact.tags.length > 0" px-2>
+      <v-flex v-if="contact.tags != 'Empty'" px-2>
         <v-flex xs1>
           <span class="field-label">Tags</span>
         </v-flex>
         <v-flex xs2>
-          <ul>
-            <li v-for="(tag, index) in contact.tags" :key="index">{{tag}}</li>
-          </ul>
+          {{contact.tags}}
         </v-flex>
       </v-flex>
     </v-layout>
@@ -103,37 +102,73 @@
 </template>
 
 <script>
+import RestServices from "../services/RestServices";
+
 export default {
   name: "ContactDetails",
-
-  // Mocked data for later API integration
   data: () => ({
     contact: {
-      name: "Carlos Andres Saavedra Regalado",
-      company: "Arkusnexus",
-      email: "csaavedra@arkusnexus.com",
-      mobile_phone: "(664) 111-1111",
-      home_phone: "(664) 222-2222",
-      birthday: "01/01/1990",
-      office_phone: "(664) 111-1111",
-      fax: "(664) 222-2222",
-      tags: []
+      name: "",
+      company: "",
+      email: "",
+      mobile_phone: "",
+      home_phone: "",
+      birthday: "",
+      office_phone: "",
+      fax: "",
+      tags: ""
     }
-  })
+  }),
+  mounted() {
+    const restServices = new RestServices();
+
+    restServices.getContact(this.$route.params.id).then(res => {
+      const empty = "Empty";
+      const contact = res.data;
+
+      this.contact.name = `${contact.first_name} ${contact.last_name}`;
+      this.contact.company = contact.company || empty;
+      this.contact.email = contact.email || empty;
+      this.contact.mobile_phone = contact.mobile_phone || empty;
+      this.contact.home_phone = contact.home_phone || empty;
+      this.contact.birthday = contact.birthday || empty;
+      this.contact.office_phone = contact.office_phone || empty;
+      this.contact.fax = contact.fax || empty;
+      this.contact.tags = contact.tags || empty;
+    });
+  },
+  methods: {
+    handleBack: function() {
+      this.$router.push("/");
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.form-title {
-  color: #2dc2e7;
+$primary: #ffffff;
+
+.container.container-details {
+  height: calc(100% - 64px);
 }
+
+.form-title {
+  color: $primary;
+}
+
 .field-label {
   font-weight: bold;
   font-size: 16px;
-  color: #d0d0d0;
+  color: $primary;
 }
+
 .field-value {
-  border-bottom: 1px solid #d0d0d0;
+  border-bottom: 1px solid $primary;
   min-height: 40px;
+}
+
+.back-icon {
+  margin-right: 10px;
+  padding-bottom: 5px;
 }
 </style>
