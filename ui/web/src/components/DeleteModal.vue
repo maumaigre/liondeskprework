ddc5b572fcb82bd2d233b64918da99bbe6df2359
Antonio Salgado <jsalgado@arkusnexus.com>
2019-08-13 12:49:19 -0700

    deleted spaces and changed dialog prop name

--- ui/web/src/components/DeleteModal.vue
+++ ui/web/src/components/DeleteModal.vue
@@ -1,29 +1,23 @@
 <template>
     <div>
       <v-row justify="center">
-
-        <v-dialog v-model="dialog" max-width="400">
+        <v-dialog v-model="showDialog" max-width="400">
           <v-card>
-
             <v-card-title class="headline">Delete contact confirmation</v-card-title>
             <v-card-text>
               Are you sure you want to delete {{name}} from your contacts list?
             </v-card-text>
-
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="normal" @click="dialog = false">
+              <v-btn color="normal" @click="showDialog = false">
                 Cancel
               </v-btn>
-
               <v-btn color="error" @click="handleDelete">
                 Yes, delete
               </v-btn>
             </v-card-actions>
-
           </v-card>
         </v-dialog>
-
       </v-row>
     </div>
 </template>
@@ -36,13 +30,13 @@ export default {
         }
     },
     props:{
-        dialog: Boolean,
+        showDialog: Boolean,
         name: String
     },
     methods:{
         handleDelete() {
             this.$emit('onDeleteContact');
-            this.dialog = false;
+            this.showDialog = false;
         }
     }
     
