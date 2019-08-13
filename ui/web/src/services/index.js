b23e4b5ce3458211fb7f3593ef4e4c0e4b4e5af8
Miguel Diaz <mdiaz@arkusnexus.com>
2019-08-13 13:02:31 -0700

    Service and Env file

--- ui/web/src/services/index.js
+++ ui/web/src/services/index.js
@@ -1,12 +1,15 @@
 
 import axios from 'axios';
 
-export default class Services{
-    createContact (contact){
-        return axios.post("https://api-v2.liondesk.com/contacts", 
-        contact,
-        { 
+export default class RestServices{
+    constructor(){
+        this.http = axios.create({
+            baseUrl: process.env.VUE_APP_BASE_URL,
             headers:{ Authorization: 'Bearer' + proccess.env.VUE_APP_ACCESS_TOKEN }
-        });
+        })
+    }
+
+    createContact (contact){
+        return axios.post("contacts", contact);
     }
 };
