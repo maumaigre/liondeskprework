7a935a31f46fd3c2f71d112779a7ccfa6c7376f8
Antonio Salgado <jsalgado@arkusnexus.com>
2019-08-13 15:48:35 -0700

    adding delete contact handler on contact card

--- ui/web/src/components/ContactList/ContactCard.vue
+++ ui/web/src/components/ContactList/ContactCard.vue
@@ -1,25 +1,48 @@
 <template>
-	<v-card @click="handleCardClick()" class="contact-card">
+	<div>
+		<DeleteModal :showModal="showModal" :name="contact.first_name" @onDeleteContact="deleteContact"></DeleteModal>
+		<v-card @click="handleCardClick()" class="contact-card">
 		<v-card-title class="title">{{contact.first_name}} {{contact.last_name}}</v-card-title>
 		<v-card-text>{{contact.email}}</v-card-text>
 		<v-card-text>{{contact.mobile_phone}}</v-card-text>
 		<v-card-actions class="actions">.
 			<v-btn text>Edit</v-btn>
-			<v-btn text>Delete</v-btn>
+			<v-btn text @click.stop="showModal">Delete</v-btn>
 		</v-card-actions>
    </v-card>
+	</div>
+	
 </template>
 
 <script>
+import DeleteModal from '../DeleteModal';
+import RestServices from '../../services/RestServices/index';
+
 export default {
 	name: "ContactCard",
+	data(){
+		return{
+			showModal: false
+		}
+	},
 	props: {
 		contact: Object
 	},
 	methods: {
 		handleCardClick(){
 			this.$router.push(`/contact/${this.contact.id}`)
-		}
+		},
+		deleteContact(){
+			const restService = new RestServices();
+			restService.deleteContact(this.contact.id)
+			.then( res => {
+				console.log(res);
+				this.$emit('updateContacts');
+			})
+			.catch( error => console.error(error));
+			this.showModal  = false;
+    	}
+
 	}
 };
 </script>
